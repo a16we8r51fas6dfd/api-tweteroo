9f14292b2d10a09d242b69be86808cf442dfc4ab
fernandinho <fernando_yudi_yaeda@hotmail.com>
2022-01-24 04:16:35 -0300

    feat:  add get tweets

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.use(cors())
 app.use(json())
 
 const userData = []
+const tweets = [
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
+	}
+]
 
 app.post('/sign-up', (req, res) => {
     if(req.body.username && req.body.avatar) {
@@ -14,6 +21,10 @@ app.post('/sign-up', (req, res) => {
     } else {
         res.status(400).send('verifique se os campos estão preenchidos corretamente')
     }
+})
+
+app.get('/tweets', (req, res) => {
+    res.send(tweets.slice(Math.max(tweets.length - 10, 0)))
 })
 
 app.listen(5000)
